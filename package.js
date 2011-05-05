854bce767c41e815435469d4daafd11328fd89ee
Troy Goode <troygoode@gmail.com>
2011-05-05 16:08:04 -0400

    added initial index.js file and a package.js file describing the package

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "shuffle",
+  "description": "?",
+  "version": "0.1.0",
+  "homepage": "https://github.com/troygoode/shuffle/",
+  "repository": {
+    "type": "git",
+    "url": "git://github.com/troygoode/shuffle.git"
+  },
+  "author": "Troy Goode <troygoode@gmail.com> (https://github.com/troygoode/)",
+  "main": "./index.js",
+  "directories": {
+    "lib": "lib"
+  },
+  "engines": {
+    "node": "0.4.x"
+  }
+}
