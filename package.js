bb10c521d456d1878b1230e088ca9b413cc9e0f3
Troy Goode <troygoode@gmail.com>
2011-05-06 13:10:30 -0400

    pointed package file at new github URL

--- package.js
+++ package.js
@@ -2,10 +2,10 @@
   "name": "shuffle",
   "description": "?",
   "version": "0.1.0",
-  "homepage": "https://github.com/troygoode/shuffle/",
+  "homepage": "https://github.com/troygoode/node-shuffle/",
   "repository": {
     "type": "git",
-    "url": "git://github.com/troygoode/shuffle.git"
+    "url": "git://github.com/troygoode/node-shuffle.git"
   },
   "author": "Troy Goode <troygoode@gmail.com> (https://github.com/troygoode/)",
   "main": "./index.js",
