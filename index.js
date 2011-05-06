a621c47c5448ca3888572b37de1eba99132d8335
Troy Goode <troygoode@gmail.com>
2011-05-06 13:02:21 -0400

    can now use other randomize libraries (like srand) for shuffling

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ var util = require('util');
 var deck = require('./lib/deck');
 var playingCardDeck = require('./lib/playingCardDeck');
 var shuffle = module.exports = {};
+var defaultOptions = {
+  deck: new playingCardDeck().cards,
+  random: function(){ return Math.random(); }
+};
 
 shuffle.playingCards = function(){
   return new playingCardDeck().cards;
 }
 
-shuffle.shuffle = function(customDeck, seed){
-  if(!customDeck)
-    customDeck = this.playingCards();
-  return new deck(customDeck, seed);
+shuffle.shuffle = function(options){
+  if(!options)
+    options = defaultOptions;
+
+  if(!options.deck)
+    options.deck = defaultOptions.deck;
+  if(!options.random)
+    options.random = defaultOptions.random;
+  
+  return new deck(options.deck, options.random);
 };
