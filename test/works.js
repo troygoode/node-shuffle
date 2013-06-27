717bc664c30e32eea0ca11c3ff532cc484548d2a
Troy Goode <troygoode@gmail.com>
2013-06-26 20:56:38 -0400

    fix issue #2. bump version

--- test/works.js
+++ test/works.js
@@ -7,6 +7,7 @@ describe('shuffle', function(){
   it('draw() works', function(){
     var deck = shuffle.shuffle({ random: rnd });
     var card = deck.draw();
+    should.not.exist(card.length);
     card.toShortDisplayString().should.equal('2D');
     deck.length.should.equal(51);
   });
@@ -14,8 +15,28 @@ describe('shuffle', function(){
   it('draw(2) works', function(){
     var deck = shuffle.shuffle({ random: rnd });
     var cards = deck.draw(2);
+    should.exist(cards.length);
+    cards.length.should.equal(2);
     cards[0].toShortDisplayString().should.equal('2D');
     cards[1].toShortDisplayString().should.equal('AC');
+    deck.length.should.equal(50);
+  });
+
+  it('drawRandom() works', function(){
+    var deck = shuffle.shuffle({ random: rnd });
+    var card = deck.drawRandom();
+    should.not.exist(card.length);
+    card.toShortDisplayString().should.equal('3D');
+    deck.length.should.equal(51);
+  });
+
+  it('drawRandom(2) works', function(){
+    var deck = shuffle.shuffle({ random: rnd });
+    var cards = deck.drawRandom(2);
+    should.exist(cards.length);
+    cards.length.should.equal(2);
+    cards[0].toShortDisplayString().should.equal('3D');
+    cards[1].toShortDisplayString().should.equal('AS');
     deck.length.should.equal(50);
   });
 
